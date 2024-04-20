935d57d81d2fa28400010f0dc2d9ecf8972690ca
SherifOmarAbdelaziz <sherif.omar.abdelaziz.2013@gmail.com>
2024-04-20 23:46:24 +0200

    Update ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -14,19 +14,18 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-SherifOmarAbdelAziz.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules/
-LDD_MODULE_SUBDIRS += scull/
+LDD_MODULE_SUBDIRS = misc-modules/ scull/
 
 
-define LDD_INSTALL_TARGET_CMDS
+#define LDD_INSTALL_TARGET_CMDS
 #	$(INSTALL) -m 0755 $(@D)/scull/*.ko $(TARGET_DIR)/usr/bin	
 #	$(INSTALL) -m 0755 $(@D)/misc-modules/*.ko $(TARGET_DIR)/usr/bin
 	
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/scull_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/scull_unload $(TARGET_DIR)/usr/bin
-endef
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/scull_load $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/scull_unload $(TARGET_DIR)/usr/bin
+#endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
